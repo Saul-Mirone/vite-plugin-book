648ddea225fadf8a254c7ce97237b74169ad290d
Mirone Saul <Saul-Mirone@outlook.com>
2022-01-24 10:25:13 +0800

    feat: 🎸 adjust the order of toolbar button

--- packages/vite-plugin-book/src/component/Toolbar.tsx
+++ packages/vite-plugin-book/src/component/Toolbar.tsx
@@ -6,11 +6,11 @@ import { SurfaceButton } from './Button';
 export const Toolbar: FC = () => {
     return (
         <div className="shadow-lg rounded-28px fixed right-24px bottom-24px bg-surface p-12px flex gap-8px">
-            <SurfaceButton icon="check_circle" />
-            <SurfaceButton icon="cancel" />
+            <SurfaceButton icon="add" />
             <SurfaceButton icon="delete" />
             <SurfaceButton icon="visibility" />
-            <SurfaceButton icon="add" />
+            <SurfaceButton icon="check_circle" />
+            <SurfaceButton icon="cancel" />
         </div>
     );
 };
