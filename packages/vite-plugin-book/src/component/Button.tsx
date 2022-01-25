d089fcc02dc363ef39a12d37dc3dcdd316cb1992
Mirone <Saul-Mirone@outlook.com>
2022-01-25 18:59:40 +0800

    fix: button style issue

--- packages/vite-plugin-book/src/component/Button.tsx
+++ packages/vite-plugin-book/src/component/Button.tsx
@@ -4,7 +4,7 @@ import type { FC } from 'react';
 export const Button: FC<{ icon: string; text: string; onClick: () => void }> = ({ icon, text, onClick }) => (
     <button
         onClick={onClick}
-        className="cursor-pointer transition appearance-none flex items-center gap-8px py-8px pr-16px pl-8px border border-solid bg-transparent border-secondary rounded-100px text-sm text-secondary hover:border-primary hover:text-primary hover:bg-background"
+        className="cursor-pointer transition appearance-none flex items-center gap-4px py-8px pr-16px pl-8px border border-solid bg-transparent border-secondary rounded-100px text-sm text-secondary hover:border-primary hover:text-primary hover:bg-background"
     >
         <span className="material-icons-outlined text-sm">{icon}</span>
         <span className="text-xs">{text}</span>
