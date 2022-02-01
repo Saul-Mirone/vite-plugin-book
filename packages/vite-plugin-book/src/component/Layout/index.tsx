cf343f2cf9a4636f2e6cab2966b60ceb6b86c2f5
Mirone Saul <Saul-Mirone@outlook.com>
2022-02-01 21:47:32 +0800

    fix: 🐛 menubar draggable

--- packages/vite-plugin-book/src/component/Layout/index.tsx
+++ packages/vite-plugin-book/src/component/Layout/index.tsx
@@ -1,17 +1,20 @@
 /* Copyright 2021, vite-plugin-book by Mirone. */
 import './style.css';
 
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import SplitPane from 'react-split-pane';
 
 import { useUIConfig } from '../../hook/useUIConfig';
 
 export const Layout: FC = ({ children }) => {
     const { menuFold } = useUIConfig();
+    const [onDrag, setOnDrag] = useState(false);
 
     return (
         <div className="h-screen w-screen overflow-hidden font-sans">
             <SplitPane
+                onDragStarted={() => setOnDrag(true)}
+                onDragFinished={() => setOnDrag(false)}
                 className="bg-background"
                 split="vertical"
                 defaultSize={menuFold ? 0 : 280}
@@ -19,11 +22,11 @@ export const Layout: FC = ({ children }) => {
                 maxSize={menuFold ? 0 : 400}
                 paneStyle={{
                     overflow: 'hidden',
-                    transition: 'all 0.2s',
+                    transition: onDrag ? '' : 'all 0.2s',
                 }}
                 pane1Style={{
                     transform: menuFold ? 'translateX(-100%)' : '',
-                    transition: 'all 0.2s',
+                    transition: onDrag ? '' : 'all 0.2s',
                 }}
             >
                 {children}
