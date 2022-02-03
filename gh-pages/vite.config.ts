2b5c63fa7280c02d8e6529601dcc5bafc4eff45a
Mirone Saul <Saul-Mirone@outlook.com>
2022-02-03 12:18:27 +0800

    feat: 🎸 include 404 when building new

--- gh-pages/vite.config.ts
+++ gh-pages/vite.config.ts
@@ -1,5 +1,7 @@
 /* Copyright 2021, vite-plugin-book by Mirone. */
 
+import path from 'path';
+import copy from 'rollup-plugin-copy';
 import { defineConfig } from 'vite';
 // eslint-disable-next-line import/no-unresolved
 import { book } from 'vite-plugin-book/vite';
@@ -7,5 +9,16 @@ import { book } from 'vite-plugin-book/vite';
 // https://vitejs.dev/config/
 export default defineConfig({
     base: '/vite-plugin-book/',
-    plugins: [book()],
+    plugins: [
+        book(),
+        copy({
+            targets: [
+                {
+                    src: [path.resolve(__dirname, '404.html')],
+                    dest: path.resolve(__dirname, './dist'),
+                },
+            ],
+            hook: 'writeBundle',
+        }),
+    ],
 });
