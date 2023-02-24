2c09a3e1f33972fb42d2d82d285693b57816e146
Mirone Saul <Saul-Mirone@outlook.com>
2023-02-24 21:28:31 +0800

    feat: 🎸 add window resize listener

--- packages/vite-plugin-book/src/provider/UIProvider.tsx
+++ packages/vite-plugin-book/src/provider/UIProvider.tsx
@@ -1,6 +1,6 @@
 /* Copyright 2021, vite-plugin-book by Mirone. */
 
-import { createContext, Dispatch, FC, memo, ReactNode, SetStateAction, useEffect, useRef, useState } from 'react';
+import { createContext, Dispatch, FC, memo, ReactNode, SetStateAction, useEffect, useState } from 'react';
 
 import { nope } from '../utils/helper';
 
@@ -11,26 +11,35 @@ export const IsDarkModeCtx = createContext<boolean>(false);
 export const SetIsDarkModeCtx = createContext<Dispatch<SetStateAction<boolean>>>(nope);
 export const IsMobileCtx = createContext<boolean>(false);
 
-const isDark = Boolean(window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches);
+const isDark = Boolean(
+    typeof window !== 'undefined' && window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches,
+);
 
-const screenWidth = document.body.getBoundingClientRect().width;
+const isMob = Boolean(typeof window !== 'undefined' && document.body.getBoundingClientRect().width < 905);
 
 export const UIProvider: FC<{ children: ReactNode }> = memo(({ children }) => {
-    const isMobile = useRef(screenWidth < 905);
-    const [menuFold, setMenuFold] = useState(isMobile.current);
+    const [isMobile, setIsMobile] = useState(isMob);
+    const [menuFold, setMenuFold] = useState(isMobile);
     const [isDarkMode, setIsDarkMode] = useState(isDark);
 
     useEffect(() => {
+        if (typeof window === 'undefined') return;
         const media = window.matchMedia('(prefers-color-scheme: dark)');
         const listener = (event: MediaQueryListEvent) => {
             const newColorScheme = !!event.matches;
 
             setIsDarkMode(newColorScheme);
         };
+        const onResize = () => {
+            const screenWidth = document.body.getBoundingClientRect().width;
+            setIsMobile(screenWidth < 905);
+        };
 
         media.addEventListener('change', listener);
+        window.addEventListener('resize', onResize);
         return () => {
             media.removeEventListener('change', listener);
+            window.removeEventListener('resize', onResize);
         };
     }, []);
 
@@ -39,7 +48,7 @@ export const UIProvider: FC<{ children: ReactNode }> = memo(({ children }) => {
     }, [isDarkMode]);
 
     return (
-        <IsMobileCtx.Provider value={isMobile.current}>
+        <IsMobileCtx.Provider value={isMobile}>
             <IsDarkModeCtx.Provider value={isDarkMode}>
                 <SetIsDarkModeCtx.Provider value={setIsDarkMode}>
                     <MenuFoldCtx.Provider value={menuFold}>
